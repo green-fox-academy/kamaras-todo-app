75b2395f50c2cea9742a0cecce2fecb43e766409
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-14 12:01:08 +0100

    Usage info file

---
+++ usage_information.py
@@ -0,0 +1,14 @@
+def print_usage_information():
+    print(
+        '\n'
+        'Command Line Todo application\n'
+        '=============================\n'
+        '\n'        
+        'Command Line arguments\n'
+        '-l   Lists all the tasks\n'
+        '-a   Adds a new task\n'
+        '-r   Removes a task\n'
+        '-c   Completes a task'
+    )
+
+# print_usage_information()
