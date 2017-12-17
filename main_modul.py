be16142877bae1224d7e27bb2c254b902e36086b
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-17 03:28:03 +0100

    Classes implemented, remove errors solved

--- main_modul.py
+++ main_modul.py
@@ -1,48 +1,57 @@
 import sys
-from usage_information import *
+from usage_information import Usage
 
-def eat_userinput():
-    if sys.argv[-1] == 'main_modul.py':
-        print_usage_information()
-    elif sys.argv[1] == '-l':
-        print_list()
-    elif sys.argv[-1] == '-a' and len(sys.argv) == 2:
-        print('Unable to add: no task provided')
-    elif sys.argv[1] == '-a' and len(sys.argv) > 2:
-        add_task()
-    elif sys.argv[1] == '-r':
-        remove_task()
-    elif sys.argv[1] == '-c':
-        print('Completes a task')
-    else:
-        print('\nType a valid argument!')
-        print_usage_information()
+class Todo():
 
-def print_list():
-    temp = open('list.txt', 'r')
-    tasks = temp.readlines()
-    if len(tasks) < 1:
-        print('No todos for today!')
-    for i in range(len(tasks)):
-        print(str(i + 1) + ' - ' + tasks[i], end = '')
-    temp.close()
+    def __init__(self):
+        pass
 
-def add_task():
-    temp = open('list.txt', 'a')
-    temp.write('\n')    
-    temp.write(sys.argv[2])
-    temp.close()
+    def eat_userinput(self):
+        if sys.argv[-1] == 'main_modul.py':
+            Usage.print_usage_information(self)
+        elif sys.argv[1] == '-l':
+            self.print_list()
+        elif sys.argv[-1] == '-a' and len(sys.argv) == 2:
+            print('Unable to add: no task provided')
+        elif sys.argv[1] == '-a' and len(sys.argv) > 2:
+            self.add_task()
+        elif sys.argv[-1] == '-r' and len(sys.argv) == 2:
+            print('Unable to remove: no index provided')
+        elif sys.argv[1] == '-r' and len(sys.argv) > 2 and sys.argv[2].isalpha():
+            print('Unable to remove: index is not a number')
+        elif sys.argv[1] == '-r' and len(sys.argv) > 2:
+            self.remove_task()
+        elif sys.argv[1] == '-c':
+            print('Completes a task')
+        else:
+            print('\nType a valid argument!')
+            print_usage_information()
 
-def remove_task():
-    temp = open('list.txt', 'r')
-    tasks = temp.readlines()
-    to_remove = tasks[int(sys.argv[2]) - 1]
-    temp.close()
+    def print_list(self):
+        temp = open('list.txt', 'r')
+        tasks = temp.readlines()
+        if len(tasks) < 1:
+            print('No todos for today!')
+        for i in range(len(tasks)):
+            print(str(i + 1) + ' - ' + tasks[i], end = '')
+        temp.close()
 
-    temp = open('list.txt', 'w')
-    for line in tasks:
-        if line != to_remove:
-            temp.write(line)
-    temp.close()
-            
-eat_userinput()
+    def add_task(self):
+        temp = open('list.txt', 'a')
+        temp.write('\n')    
+        temp.write(sys.argv[2])
+        temp.close()
+
+    def remove_task(self):
+        temp = open('list.txt', 'r')
+        tasks = temp.readlines()
+        to_remove = tasks[int(sys.argv[2]) - 1]
+        temp.close()
+        temp = open('list.txt', 'w')
+        for line in tasks:
+            if line != to_remove:
+                temp.write(line)
+        temp.close()
+
+todo = Todo()
+todo.eat_userinput()
--- usage_information.py
+++ usage_information.py
@@ -1,14 +1,19 @@
-def print_usage_information():
-    print(
-        '\n'
-        'Command Line Todo application\n'
-        '=============================\n'
-        '\n'        
-        'Command Line arguments\n'
-        '-l   Lists all the tasks\n'
-        '-a   Adds a new task\n'
-        '-r   Removes a task\n'
-        '-c   Completes a task'
-    )
+class Usage():
+
+    def __init__(self):
+        pass
+
+    def print_usage_information(self):
+        print(
+            '\n'
+            'Command Line Todo application\n'
+            '=============================\n'
+            '\n'        
+            'Command Line arguments\n'
+            '-l   Lists all the tasks\n'
+            '-a   Adds a new task\n'
+            '-r   Removes a task\n'
+            '-c   Completes a task'
+        )
 
 # print_usage_information()
