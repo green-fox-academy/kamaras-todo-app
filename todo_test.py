8e9d10bd64d891976f169648866bc38017f284f0
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-17 04:11:19 +0100

    Start unittests

---
+++ todo_test.py
@@ -0,0 +1,12 @@
+import unittest
+import usage_information
+# import main_modul
+
+class TextUsage(unittest.TestCase):
+    pass
+
+class MainModulTest(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
